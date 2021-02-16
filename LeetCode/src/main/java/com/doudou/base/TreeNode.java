8c9316a4d461745fd2fded0bbb2bebc5993960a1
coderlmm <coderlmm@gmail.com>
2021-02-16 22:10:41 +0800

    leetcode

---
+++ LeetCode/src/main/java/com/doudou/base/TreeNode.java
@@ -0,0 +1,13 @@
+package com.doudou.base;
+
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
+
---
+++ LeetCode/src/main/java/com/doudou/questions/Question_1.java
@@ -0,0 +1,25 @@
+package com.doudou.questions;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ * <p>
+ * https://leetcode-cn.com/problems/two-sum/
+ */
+public class Question_1 {
+    public int[] twoSum(int[] nums, int target) {
+        // value:index
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
