6ee8d9840a650c84bc0e21b27b59f3b8512b1da1
coderlmm <coderlmm@gmail.com>
2021-02-18 23:15:32 +0800

    leetcode

--- LeetCode/src/main/java/com/doudou/base/ListNode.java
+++ LeetCode/src/main/java/com/doudou/base/ListNode.java
@@ -5,7 +5,15 @@ public class ListNode {
     public int val;
     public ListNode next;
 
+    public ListNode() {
+    }
+
     public ListNode(int x) {
         val = x;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
---
+++ LeetCode/src/main/java/com/doudou/questions/Question_2.java
@@ -0,0 +1,40 @@
+package com.doudou.questions;
+
+import com.doudou.base.ListNode;
+
+/**
+ * 给你两个非空 的链表，表示两个非负的整数。它们每位数字都是按照逆序的方式存储的，并且每个节点只能存储一位数字。
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * <p>
+ * https://leetcode-cn.com/problems/add-two-numbers
+ */
+public class Question_2 {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return head;
+    }
+}
