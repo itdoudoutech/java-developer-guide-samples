e92815ee3fcecf5319be632dbd3528902068c0c2
coderlmm <coderlmm@gmail.com>
2021-04-02 01:00:19 +0800

    modify dependencyManagement & add rest-client module

---
+++ user-platform/rest-client/src/main/java/com/doudou/rest/DefaultRuntimeDelegate.java
@@ -0,0 +1,42 @@
+package com.doudou.rest;
+
+
+import com.doudou.rest.client.DefaultVariantListBuilder;
+import com.doudou.rest.core.DefaultResponseBuilder;
+import com.doudou.rest.core.DefaultUriBuilder;
+
+import javax.ws.rs.core.*;
+import javax.ws.rs.ext.RuntimeDelegate;
+
+public class DefaultRuntimeDelegate extends RuntimeDelegate {
+
+    @Override
+    public UriBuilder createUriBuilder() {
+        return new DefaultUriBuilder();
+    }
+
+    @Override
+    public Response.ResponseBuilder createResponseBuilder() {
+        return new DefaultResponseBuilder();
+    }
+
+    @Override
+    public Variant.VariantListBuilder createVariantListBuilder() {
+        return new DefaultVariantListBuilder();
+    }
+
+    @Override
+    public <T> T createEndpoint(Application application, Class<T> endpointType) throws IllegalArgumentException, UnsupportedOperationException {
+        return null;
+    }
+
+    @Override
+    public <T> HeaderDelegate<T> createHeaderDelegate(Class<T> type) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public Link.Builder createLinkBuilder() {
+        return null;
+    }
+}
